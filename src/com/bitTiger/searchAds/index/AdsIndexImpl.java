f3322306d1070d4d6e326cbc5d0b2da2ecad25ed
Yin Zhang <ynzhang@linkedin.com>
2016-04-22 07:00:40 +0000

    ads search skeleton

--- src/com/bitTiger/searchAds/index/AdsIndexImpl.java
+++ src/com/bitTiger/searchAds/index/AdsIndexImpl.java
@@ -23,33 +23,38 @@ public class AdsIndexImpl implements AdsIndex {
 
   @Override
   public List<AdsStatsInfo> indexMatch(List<String> keyWords) {
-    Iterator<String> keywordsIterator = keyWords.iterator();
-    Map<Integer, Integer> hitCounts = new HashMap<Integer, Integer>();
-    while (keywordsIterator.hasNext()) {
-      String keyWord = keywordsIterator.next();
-      List<Integer> matchedAdsIds = _adsInvertedIndex.retrieveIndex(keyWord);
-      Iterator<Integer> matchedAdsIdsIterator = matchedAdsIds.iterator();
-      while (matchedAdsIdsIterator.hasNext()) {
-        Integer matchedAdsId = matchedAdsIdsIterator.next();
-        hitCounts.put(matchedAdsId, hitCounts.get(matchedAdsId) == null ? 1 : hitCounts.get(matchedAdsId) + 1);
-      }
-    }
     List<AdsStatsInfo> adsStatsInfoList = new ArrayList<AdsStatsInfo>();
-    Iterator<Map.Entry<Integer, Integer>> hitCountsIterator = hitCounts.entrySet().iterator();
-    while (hitCountsIterator.hasNext()) {
-      Map.Entry<Integer, Integer> hitCountsEntry = hitCountsIterator.next();
-      Integer adsId = hitCountsEntry.getKey();
-      Integer hitCount = hitCountsEntry.getValue();
-      AdsCampaignInfo adsInfo = _adsForwardedIndex.findAds(adsId);
-      if (adsInfo.getBudget() > 0) {
-        AdsStatsInfo adsStatsInfo = new AdsStatsInfo(adsId);
-        adsStatsInfo.setRelevanceScore(hitCount*1.0f/_adsForwardedIndex.findAds(adsId).getAdsKeyWords().size());
-        adsStatsInfoList.add(adsStatsInfo);
+    if (keyWords != null) {
+      Iterator<String> keywordsIterator = keyWords.iterator();
+      Map<Integer, Integer> hitCounts = new HashMap<Integer, Integer>();
+      while (keywordsIterator.hasNext()) {
+        String keyWord = keywordsIterator.next();
+        List<Integer> matchedAdsIds = _adsInvertedIndex.retrieveIndex(keyWord);
+        if (matchedAdsIds != null) {
+          Iterator<Integer> matchedAdsIdsIterator = matchedAdsIds.iterator();
+          while (matchedAdsIdsIterator.hasNext()) {
+            Integer matchedAdsId = matchedAdsIdsIterator.next();
+            hitCounts.put(matchedAdsId, hitCounts.get(matchedAdsId) == null ? 1 : hitCounts.get(matchedAdsId) + 1);
+          }
+        }
+      }
+      Iterator<Map.Entry<Integer, Integer>> hitCountsIterator = hitCounts.entrySet().iterator();
+      while (hitCountsIterator.hasNext()) {
+        Map.Entry<Integer, Integer> hitCountsEntry = hitCountsIterator.next();
+        Integer adsId = hitCountsEntry.getKey();
+        Integer hitCount = hitCountsEntry.getValue();
+        AdsCampaignInfo adsInfo = _adsForwardedIndex.findAds(adsId);
+        if (adsInfo != null && adsInfo.getBudget() > 0) {
+          AdsStatsInfo adsStatsInfo = new AdsStatsInfo(adsId);
+          adsStatsInfo.setRelevanceScore(hitCount*1.0f/_adsForwardedIndex.findAds(adsId).getAdsKeyWords().size());
+          adsStatsInfoList.add(adsStatsInfo);
+        }
       }
     }
     return adsStatsInfoList;
   }
 
+  @Override
   public AdsInventory buildIndex(String fileName) {
     // TODO Auto-generated method stub
     return null;
