b2ef43b7786d22497c3f850ada00afd721c59750
Andrew Jiang <ajiang@civi.ca>
2016-04-27 17:42:16 -0400

    return exception unit test

--- src/com/bitTiger/searchAds/index/AdsIndexImplUnitTest.java
+++ src/com/bitTiger/searchAds/index/AdsIndexImplUnitTest.java
@@ -1,7 +1,17 @@
 package com.bitTiger.searchAds.index;
 
+import java.io.FileNotFoundException;
+
 import org.junit.Test;
 
 public class AdsIndexImplUnitTest {
      
+	@Test(expected = FileNotFoundException.class)
+     public void ShouldReturnException() throws FileNotFoundException
+     {
+    	 AdsIndexImpl adsIndexImpl = new AdsIndexImpl();
+    	 adsIndexImpl.buildIndex("ad-data.json","CamPaign.json");
+    	 
+     }
+
 }
