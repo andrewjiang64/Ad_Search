4cc2d49a8f41131a22858b3b944b1fd53c8e4903
Andrew Jiang <zhigangjian@gmail.com>
2016-04-27 19:43:38 -0400

    junit test

--- src/com/bitTiger/searchAds/index/AdsIndexImplUnitTest.java
+++ src/com/bitTiger/searchAds/index/AdsIndexImplUnitTest.java
@@ -1,7 +1,15 @@
 package com.bitTiger.searchAds.index;
 
+import java.io.FileNotFoundException;
+
 import org.junit.Test;
 
 public class AdsIndexImplUnitTest {
-     
+    
+	@Test(expected = FileNotFoundException.class)
+	public void ShouldReturnException()
+	{
+		AdsIndexImpl adsIndexImpl = new AdsIndexImpl();
+		adsIndexImpl.buildIndex("ads-data.json", "CamPaign.json");
+	}
 }
